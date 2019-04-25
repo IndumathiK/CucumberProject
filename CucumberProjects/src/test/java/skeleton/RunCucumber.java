11fc9f2b8b687d048048aed396c59e3c6b1df987
Training_b6b.00.03 <Training_b6b.00.03@BDC6-DX-3QT0N62.dir.svc.accenture.com>
2019-04-25 16:24:19 +0530

    commit cucumber project

---
+++ CucumberProjects/src/test/java/skeleton/RunCucumber.java
@@ -0,0 +1,16 @@
+package skeleton;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin= {"pretty","html:src/cucumber-reports"},
+                 features="src/test/resources/skeleton/Admin.feature",
+                 monochrome=true)
+public class RunCucumber 
+{
+
+    
+}
